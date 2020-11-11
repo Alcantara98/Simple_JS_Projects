4b2cb7f30fe4f5d526a7146c1a7ab92493845f31
alcantara98 <elbertalcantara.98@gmail.com>
2020-11-12 01:12:43 +1300

    Finished and resolved the Async Loop Issues

--- psr.js
+++ psr.js
@@ -1,10 +1,76 @@
+const readline = require('readline');
+
 const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    userInput = userInput.toString().toLowerCase();
+    if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
         return userInput;
-    }else{
+    } else {
         console.log("Invalid Input");
     }
 }
 
-getUserChoice('roc');
+const getComputerChoice = () => {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'tie';
+    } else if (userChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            return 'user won';
+        } else {
+            return 'computer won';
+        }
+    } else if (userChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            return 'user won';
+        } else {
+            return 'computer won';
+        }
+    } else if (userChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            return 'user won';
+        } else {
+            return 'computer won';
+        }
+    }
+}
+
+const playGame = (userInput) => {
+    let userIn = getUserChoice(userInput);
+    let compIn = getComputerChoice();
+    return determineWinner(userIn, compIn);
+}
+
+
+
+async function askQuestion(query) {
+    while (true) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        const promise = new Promise(resolve => rl.question(query, userIn => {
+            rl.close();
+            resolve(playGame(userIn));
+        }))
+        const ans = await promise;
+        console.log(ans);
+    }
+}
+
+askQuestion("Pick: ");
+
+
+
+
+
+
